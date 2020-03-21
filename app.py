57e4de5e29283cc0ebd78826d3c730110fb3c7e0
Kaido <kaido.soomlais@gmail.com>
2020-03-21 15:40:26 +0000

    added bootstrap to base.html, added styles.css

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'cocktails'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-9apc7.mongodb.net/cocktails?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
+
 
 @app.route('/')
-def hello():
-    return 'Hello Cocktails'
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("home.html", recipes=mongo.db.recipes.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
