5f4641508f9a24d69834b0a4df9e39ce38696a8b
Kaido <kaido.soomlais@gmail.com>
2020-03-28 11:03:29 +0000

    created forms.py, added login register to app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
+from forms import RegistrationForm, LoginForm
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.confi['SECRET_KEY'] = ''
 app.config["MONGO_DBNAME"] = 'cocktails'
 app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-9apc7.mongodb.net/cocktails?retryWrites=true&w=majority'
 
@@ -16,6 +18,16 @@ mongo = PyMongo(app)
 @app.route('/get_recipes')
 def get_recipes():
     return render_template("home.html", recipes=mongo.db.recipes.find())
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('register.html', title='Log In', form=form)
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
