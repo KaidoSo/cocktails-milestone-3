250b51c46caf7f38b8392196bb75d3b61f6ddb9a
Kaido <kaido.soomlais@gmail.com>
2020-05-09 14:41:05 +0000

    added create, edit and delete forms to forms.py create.html created

--- forms.py
+++ forms.py
@@ -18,3 +18,21 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Log In')
+
+class CreateForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    image = StringField('Image Link (full path)', validators=[DataRequired()])
+    ingredients = TextAreaField('Ingredients(one per line)', validators=[DataRequired()])
+    instructions = TextAreaField('Instructions', validators=[DataRequired()])
+    submit = SubmitField('Add Drink')
+
+class EditForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    image = StringField('Image Link (full path)', validators=[DataRequired()])
+    ingredients = TextAreaField('Ingredients(one per line)', validators=[DataRequired()])
+    instructions = TextAreaField('Instructions', validators=[DataRequired()])
+    submit = SubmitField('Add Drink')
+
+class Delete(FlaskForm):
+    title = StringField('Name', validators=[DataRequired()])
+    submit = SubmitField('Delete this Drink')
